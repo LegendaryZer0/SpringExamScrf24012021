b944634fbf9e7d47ee4e4376a1569727250ab922
Rifdar <assasinnyx@mail.ru>
2021-01-25 01:01:03 +0300

    minority changes

--- src/main/java/ru/itis/demo/interceptors/ServiceInterceptorAppConfig.java
+++ src/main/java/ru/itis/demo/interceptors/ServiceInterceptorAppConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Component
-public class ProductServiceInterceptorAppConfig extends WebMvcConfigurerAdapter {
+public class ServiceInterceptorAppConfig extends WebMvcConfigurerAdapter {
     @Autowired
     private SecurityInterceptor securityInterceptor;
     @Autowired
